cdcd22ba928489e36a550a8134c619c22f03c050
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-08-31 09:58:24 +0200

    style min-width pour le logo Accueil

--- src/API/api.js
+++ src/API/api.js
@@ -27,7 +27,8 @@ export const getAllDatas = async (id) => {
       throw new Error(error);
     });
 };
-// call for mocked Datas
+
+/* A mocked data for the app. */
 const datas = "/mockeddatas.json";
 export const getAllDatasMocked = () => {
   return axios.get(datas).then((response) => response.data);
